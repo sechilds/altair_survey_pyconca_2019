29ae44a38566194baf6359a5f5a0f6517b6863bc
Stephen Childs <sechilds@gmail.com>
2019-11-09 08:11:38 -0500

    Got dino working

--- chart1.py
+++ chart1.py
@@ -1,17 +1,27 @@
 import pandas as pd
 import altair as alt
 
+def my_theme():
+    return {
+            'config': {
+                'range': {'category': {'scheme': 'dark2'}}
+            }
+        }
+
+alt.themes.register('my_theme', my_theme)
+alt.themes.enable('my_theme')
+
 datasaurus = pd.read_csv('data/DataSaurusDozen.tsv', delimiter = '\t')
 
-alt.Chart(datasaurus).mark_point().encode(
+alt.Chart(datasaurus[datasaurus.dataset == 'dino']).mark_point(color="#1b9e77").encode(
     x = 'x:Q',
     y = 'y:Q',
     size = alt.value(90),
     strokeWidth = alt.value(4)
-).transform_filter(alt.datum.dataset == 'dino').configure_axis(
+).configure_axis(
     labelFontSize = 24,
     titleFontSize = 36
 ).properties(
-    height = 800,
-    width = 800
-).save('slide1.html')
+    height = 600,
+    width = 600
+).save('slide1.json')
