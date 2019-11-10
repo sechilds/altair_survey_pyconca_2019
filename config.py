e3f511707bb5955af315dc23304cc4ab968e2f03
Stephen Childs <sechilds@gmail.com>
2019-11-10 14:48:35 -0500

    Add colors from dark2

--- config.py
+++ config.py
@@ -3,3 +3,13 @@ color_scale_scheme = 'dark2'
 axis_title_font_size = 36
 axis_title_font_size2 = 18
 axis_label_font_size = 24
+
+dark2_colours = ["#1b9e77",
+                 "#d95f02",
+                 "#7570b3",
+                 "#e7298a",
+                 "#66a61e",
+                 "#e6ab02",
+                 "#a6761d",
+                 "#666666"]
+
