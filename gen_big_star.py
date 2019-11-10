56023de2f7e0507dfcb17ec6d14fab78e1d7e4e1
Stephen Childs <sechilds@gmail.com>
2019-11-10 06:45:56 -0500

    Generate all charts in the datasaurus dataset.

---
+++ gen_big_star.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import altair as alt
+
+datasaurus = pd.read_csv('data/DataSaurusDozen.tsv', delimiter = '\t')
+
+alt.Chart(datasaurus[datasaurus.dataset == 'star']).mark_point(color="#1b9e77").encode(
+    x = 'x:Q',
+    y = 'y:Q',
+    size = alt.value(90),
+    strokeWidth = alt.value(4)
+).configure_axis(
+    labelFontSize = 24,
+    titleFontSize = 36
+).properties(
+    height = 600,
+    width = 600
+).save('big_star.json')
