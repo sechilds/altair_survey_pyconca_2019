c46eddcac2a2f81fffd33ac248c401809a9351cc
Stephen Childs <sechilds@gmail.com>
2019-11-10 11:57:55 -0500

    Nice looking bar charts

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 color_scale_scheme = 'dark2'
 
-axis_title_font = 36
-axis_label_font = 24
+axis_title_font_size = 36
+axis_title_font_size2 = 18
+axis_label_font_size = 24
--- gen_div_bar1.py
+++ gen_div_bar1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import altair as alt
 
-from config import color_scale_scheme, axis_label_font, axis_title_font
+from config import color_scale_scheme, axis_label_font_size, axis_title_font_size, axis_title_font_size2
 
 
 df = pd.read_csv('data/response_data1.csv')
@@ -10,10 +10,16 @@ alt.Chart(df).mark_bar().encode(
         x = 'count:Q',
         y = 'response:N',
         fill = alt.Fill('response:N', scale = alt.Scale(scheme = color_scale_scheme))
+).properties(
+        height = 250,
+        width = 400
 ).facet(
     row = 'field_name:N'
 ).configure_axis(
-        labelFontSize = axis_label_font
-        titleFontSize = axis_title_font
+        labelFontSize = axis_label_font_size,
+        titleFontSize = axis_title_font_size2
+).configure_header(
+        labelFontSize = axis_label_font_size,
+        titleFontSize = axis_title_font_size
 ).save('div_bar1.json')
 
