238ed9125086fbc82aecff0b9f1a453bdb2a5ecc
Stephen Childs <sechilds@gmail.com>
2019-11-10 11:29:41 -0500

    work on bar chart

---
+++ config.py
@@ -0,0 +1,4 @@
+color_scale_scheme = 'dark2'
+
+axis_title_font = 36
+axis_label_font = 24
--- gen_div_bar1.py
+++ gen_div_bar1.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import altair as alt
 
+from config import color_scale_scheme, axis_label_font, axis_title_font
+
+
 df = pd.read_csv('data/response_data1.csv')
 
 alt.Chart(df).mark_bar().encode(
         x = 'count:Q',
         y = 'response:N',
-        fill = alt.Fill('response:N', scale = alt.Scale(scheme = 'dark2'))
+        fill = alt.Fill('response:N', scale = alt.Scale(scheme = color_scale_scheme))
 ).facet(
     row = 'field_name:N'
+).configure_axis(
+        labelFontSize = axis_label_font
+        titleFontSize = axis_title_font
 ).save('div_bar1.json')
 
