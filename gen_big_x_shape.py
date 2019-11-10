018ed576908de1c2bcf4c5604fb30eaa32b57f81
Stephen Childs <sechilds@gmail.com>
2019-11-10 15:42:20 -0500

    Remove some extra datasaurus plots.

--- gen_big_x_shape.py
+++ gen_big_x_shape.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import altair as alt
 
+from config import dark2_colours, axis_label_font_size, axis_title_font_size
+
 datasaurus = pd.read_csv('data/DataSaurusDozen.tsv', delimiter = '\t')
 
-alt.Chart(datasaurus[datasaurus.dataset == 'x_shape']).mark_point(color="#1b9e77").encode(
+alt.Chart(datasaurus[datasaurus.dataset == 'x_shape']).mark_point(color=dark_2_colours[2]).encode(
     x = 'x:Q',
     y = 'y:Q',
     size = alt.value(90),
     strokeWidth = alt.value(4)
 ).configure_axis(
-    labelFontSize = 24,
-    titleFontSize = 36
+    labelFontSize = axis_label_font_size,
+    titleFontSize = axis_title_font_size 
 ).properties(
     height = 600,
     width = 600
